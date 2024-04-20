dc6d39bb17153c43e0aa53b4af2431c15fc05730
mazibukofp <mazibukofp@gmail.com>
2024-04-20 09:04:57 +0200

    added axios and api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+function weatherFocast(response) {
+  let tempElement = document.querySelector("#temperature");
+  let temp = Math.round(response.data.temperature.current);
+  tempElement.innerHTML = temp;
+}
+
+function searchEngine(city) {
+  let apiKey = "64ff4bb3f1cb03t54443a9aod8ab5582";
+  let weatherUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(weatherUrl).then(weatherFocast);
+}
 function searchButton(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-button");
-  console.log(searchInput);
-  let city = document.querySelector("h1");
+  let city = document.querySelector("#city");
   city.innerHTML = searchInput.value;
+  searchEngine(searchInput.value);
 }
 let searchElement = document.querySelector("#search-input-form");
 searchElement.addEventListener("submit", searchButton);
